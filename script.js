1e5fa7f7e29705f5ac8239bcc2558f1b4798d95e
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-08-22 14:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let splitStr = s.trim().split(" ");
+	return splitStr[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s))
+
+
